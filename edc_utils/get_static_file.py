933fed3adeff1b9a410b72326ed09ae961e1bf16
erikvw <ew2789@gmail.com>
2019-06-11 19:05:10 -0500

    just return the path or url

--- edc_utils/get_static_file.py
+++ edc_utils/get_static_file.py
@@ -6,20 +6,19 @@ from urllib.error import URLError
 
 
 def get_static_file(app_label, filename):
-    path = os.path.join(
-        settings.STATIC_ROOT, app_label, filename)
+    path = os.path.join(settings.STATIC_ROOT, app_label, filename)
     try:
         with open(path, "r"):
             pass
     except FileNotFoundError:
-        url = os.path.join(
-            f"https://{settings.STATIC_URL}", app_label, filename)
+        path = os.path.join(f"https://{settings.STATIC_URL}", app_label, filename)
         try:
-            path, _ = urllib.request.urlretrieve(url)
+            urllib.request.urlretrieve(path)
         except URLError as e:
             raise FileNotFoundError(
                 f"Static file not found. "
                 f"Tried STATIC_ROOT ({settings.STATIC_ROOT}) and "
                 f"STATIC_URL ({settings.STATIC_URL}). "
-                f"Got {app_label}/{filename}.")
+                f"Got {app_label}/{filename}."
+            )
     return path
