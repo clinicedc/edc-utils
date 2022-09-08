54dfedd845809c109640222155d4372613216bf9
erikvw <ew2789@gmail.com>
2022-09-07 20:56:11 -0500

    add floor/ceil for dates

--- edc_utils/date.py
+++ edc_utils/date.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from datetime import date, datetime
 from zoneinfo import ZoneInfo
 
@@ -17,3 +19,13 @@ def get_utcnow_as_date() -> date:
 def to_utc(dt: datetime) -> datetime:
     """Returns UTC datetime from any aware datetime."""
     return dt.astimezone(ZoneInfo("UTC"))
+
+
+def floor_datetime(dt) -> datetime:
+    return dt.replace(hour=0, minute=0, second=0, microsecond=0).astimezone(ZoneInfo("UTC"))
+
+
+def ceil_datetime(dt) -> datetime:
+    return dt.replace(hour=23, minute=59, second=59, microsecond=999999).astimezone(
+        ZoneInfo("UTC")
+    )
