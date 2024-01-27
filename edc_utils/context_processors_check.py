d96144cb06746fb414755a0baa2fbad5c7d58243
erikvw <ew2789@gmail.com>
2024-01-26 23:42:14 -0600

    remove sys.write from system check

--- edc_utils/context_processors_check.py
+++ edc_utils/context_processors_check.py
@@ -1,5 +1,3 @@
-import sys
-
 from django.conf import settings
 from django.core.checks import Error
 from django.core.management import color_style
@@ -10,8 +8,6 @@ style = color_style()
 def edc_context_processors_check(
     app_configs, app_label=None, context_processor_name=None, error_code=None, **kwargs
 ):
-    msg = f"check for {app_label}.context_processor"
-    sys.stdout.write(style.SQL_KEYWORD(f"{msg} ... \r"))
     errors = []
     for template_config in settings.TEMPLATES:
         if context_processor_name not in template_config.get("OPTIONS").get(
@@ -24,5 +20,4 @@ def edc_context_processors_check(
                     id=f"{app_label}.{error_code or '001'}",
                 )
             )
-    sys.stdout.write(style.SQL_KEYWORD(f"{msg} ... done.\n"))
     return errors
--- edc_utils/dashboard_middleware_check.py
+++ edc_utils/dashboard_middleware_check.py
@@ -1,5 +1,3 @@
-import sys
-
 from django.conf import settings
 from django.core.checks import Error
 from django.core.management import color_style
@@ -10,8 +8,6 @@ style = color_style()
 def edc_middleware_check(
     app_configs, app_label=None, middleware_name=None, error_code=None, **kwargs
 ):
-    msg = f"check for {app_label}.middleware"
-    sys.stdout.write(style.SQL_KEYWORD(f"{msg} ... \r"))
     errors = []
     if middleware_name not in settings.MIDDLEWARE:
         errors.append(
@@ -20,5 +16,4 @@ def edc_middleware_check(
                 id=f"{app_label}.{error_code or '001'}",
             )
         )
-    sys.stdout.write(style.SQL_KEYWORD(f"{msg} ... done.\n"))
     return errors
